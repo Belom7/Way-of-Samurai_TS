5e7a5feedc3557b3d39f5cf38f708cdde8b41266
Belom7 <Still07pkm@gmail.com>
2021-11-17 17:59:57 +0300

    20. <NavLink> активный className, рефактор кода -> убрал дублирующий код, создал массив с данными о компоненте и методом map отрисовал новые компоненты

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import cl from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
+
+const arrNavBar = [
+    {path: '/profile', title: 'Профиль'},
+    {path: '/message', title: 'Сообщения'},
+    {path: '/news', title: 'Новости'},
+    {path: '/music', title: 'Музыка'},
+    {path: '/settings', title: 'Настройки'},
+]
 
 export const Navbar = () => {
     return (
         <div className={cl.navbar}>
-            <div className={cl.item}>
-                <a href='/profile'>Профиль</a>
+            {arrNavBar.map((el_navBar) => {
+                return (
+                    <div className={cl.item}>
+                        <NavLink to={el_navBar.path}className={({isActive}) => isActive ? cl.active : cl.a}>{el_navBar.title}</NavLink>
+                    </div>
+                )
+            })}
+            {/*<div className={cl.item}>
+                <NavLink to='/profile' className={({isActive})=> isActive ? cl.active : ''}>Профиль</NavLink>
             </div>
             <div className={cl.item}>
-                <a href='/message'>Сообщения</a>
+                <NavLink to='/message'>Сообщения</NavLink>
             </div>
             <div className={cl.item}>
-                <a href='/news'>Новости</a>
+                <NavLink to='/news'>Новости</NavLink>
             </div>
             <div className={cl.item}>
-                <a href='/music'>Музыка</a>
+                <NavLink to='/music'>Музыка</NavLink>
             </div>
             <div className={cl.item}>
-                <a href='/settings'>Настройки</a>
-            </div>
+                <NavLink to='/settings'>Настройки</NavLink>
+            </div>*/}
         </div>
     )
 }
