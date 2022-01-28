f8bc0ecb0bb7f25230b6cd9f7b502a93114bd63c
Belom7 <Still07pkm@gmail.com>
2022-01-28 16:14:04 +0300

    урок 30. Рефактор, убрал ненужные типы

--- src/Components/Body/Dialogs/Dialog/Dialog.tsx
+++ src/Components/Body/Dialogs/Dialog/Dialog.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import cl from './Dialog.module.css'
 import {NavLink} from "react-router-dom";
+import {arrNameType} from "../../../../Redux/state";
 
-type propsType = {
-    name:string
-    id:number
-}
 
-export const Dialog:React.FC<propsType> = (props) => {
+export const Dialog:React.FC<arrNameType> = (props) => {
     return (
         <div className={cl.dialogItems}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+            <NavLink to={`/message/${props.id}`}><img alt={'avatar'} className={cl.img} src={props.img}/>{props.name}</NavLink>
         </div>
     )
 }
