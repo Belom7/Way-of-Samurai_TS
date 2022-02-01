54b2fb69d34f5086931db7719f91ec04d45e1fae
Belom7 <Still07pkm@gmail.com>
2022-02-02 00:15:30 +0300

    44 - React JS Практика - Context API

--- src/storeContext.tsx
+++ src/storeContext.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {appStoreType} from "./Redux/store";
 
+
 export const StoreContext = React.createContext({} as appStoreType)
 
 type ProviderType = {
-    value: appStoreType
-    children: any
+    value:appStoreType
+    children:React.ReactNode
 }
-export const Provider = (props: ProviderType) => {
+
+export const Provider = (props:ProviderType) => {
     return (
         <StoreContext.Provider value={props.value}>
             {props.children}
